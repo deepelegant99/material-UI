44755e96fe56c8853fa39a575b64edef305af97d
Phillip Wong <deepelegant99@gmail.com>
2021-09-03 04:58:22 -0700

    output cards with info from json file

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,27 +1,26 @@
 import { Grid } from "@material-ui/core";
 import Fcard from "./Fcard";
-import {ConstantData} from "./Constant";
+import { ConstantData } from "./Constant";
 const Content = () => {
-
-
-  return(
+  return (
     <>
-      
-  {ConstantData.map((data, key) => {
-    return (
-      <div key={key}>
-        {data.name}+
-          <img src={data.picture} width="500px" height="500px"/>
-           {data.description}
-      </div>
-    );
-  })}
+      <Grid container spacing={2}> 
+        {ConstantData.map((data, key) => {
+          return (
+            <Grid item xs={12} sm={4}>
+              <Fcard
+                name={data.name}
+                picture={data.picture}
+                description={data.description}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
     </>
-  )
+  );
 };
 
 export default Content;
 
-
- 
 //Testing
--- src/components/Fcard.js
+++ src/components/Fcard.js
@@ -7,6 +7,7 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { Description } from "@material-ui/icons";
 
 const useStyles = makeStyles({
   root: {
@@ -17,34 +18,28 @@ const useStyles = makeStyles({
   },
 });
 
-const Fcard = () => {
+const Fcard = props => {
+
+  const {name, picture, description} = props;
+  console.log(name, picture, description)
   const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="https://static.scientificamerican.com/sciam/cache/file/41DF7DA0-EE58-4259-AA815A390FB37C55_source.jpg?w=590&h=800&92AD8A57-5046-4AC2-B2480CC9628B1F2E"
-          title="Contemplative Reptile"
+          image= {picture}
+          title={name}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            {name}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
     </Card>
   );
 };
