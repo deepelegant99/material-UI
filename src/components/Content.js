c2639cbc79a70158994bcb77a095931f55ee4918
Phillip Wong <deepelegant99@gmail.com>
2021-09-03 08:45:56 -0700

    created a function to output the cards

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,20 +2,18 @@ import { Grid } from "@material-ui/core";
 import Fcard from "./Fcard";
 import { ConstantData } from "./Constant";
 const Content = () => {
+
+  const getCard = jObject =>{return(
+    <Grid item xs={12} sm={4}>
+      <Fcard {...jObject} />
+    </Grid>
+  )}
+
+
   return (
     <>
-      <Grid container spacing={2}> 
-        {ConstantData.map((data, key) => {
-          return (
-            <Grid item xs={12} sm={4}>
-              <Fcard
-                name={data.name}
-                picture={data.picture}
-                description={data.description}
-              />
-            </Grid>
-          );
-        })}
+      <Grid container spacing={4}> 
+        {ConstantData.map( data => getCard(data))}
       </Grid>
     </>
   );
