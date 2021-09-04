c3545327acf99e6791f12d75b32d727247ca1345
Henry Crossley <crossh2001@yahoo.com>
2021-09-03 22:43:59 -0700

    added menu links

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,9 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import AndroidOutlinedIcon from "@material-ui/icons/AndroidOutlined";
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -21,18 +24,49 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = () => {
   const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <IconButton
+            aria-controls="simple-menu"
             edge="start"
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+
+            
+
+            onClick={handleClick}
           >
-            <MenuIcon />
+    
           </IconButton>
+
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          
+          >
+                      
+                <MenuItem onClick={handleClose}>Name</MenuItem>
+                <MenuItem onClick={handleClose}>Description</MenuItem>
+                <MenuItem onClick={handleClose}>pic</MenuItem>
+                <MenuItem onClick={handleClose}>More</MenuItem>
+            </Menu>
           <Typography variant="h6" className={classes.title}>
             Area 51
           </Typography>
